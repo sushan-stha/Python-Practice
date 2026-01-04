d99d19a5ed6fe75f2347884d369da78d1cce3598
Sushan Shrestha <shresthasushan2004@gmail.com>
2026-01-04 13:47:19 +0545

    update

---
+++ compond_interest_cal.py
@@ -0,0 +1,23 @@
+# Python coumpund interest calculator
+
+principle = 0
+rate = 0
+time = 0
+
+while principle <= 0:
+    principle = float(input("Enter the principle amount:"))
+    if principle <= 0:
+        print("Principle can't be less than or equal to zero.")
+
+while rate <= 0:
+    rate = float(input("Enter the rate :"))
+    if rate <= 0:
+        print("Rate can't be less than or equal to zero.")
+
+while time <= 0:
+    time = float(input("Enter the time period (in years) :"))
+    if time <= 0:
+        print("Years can't be less than or equal to zero.")
+
+total = principle * pow((1+rate/100), time)
+print(f"Balance after {time} years is: Rs{total:.2f}")
